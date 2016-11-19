139fce2f53973cab3e87bd536a6ea778658c5d06
= <leolewin@live.cn>
2016-11-19 23:46:44 +0800

    把路由抽取出来

--- meadowlark.js
+++ meadowlark.js
@@ -5,6 +5,7 @@ var fortune = require('./public/lib/fortune.js');
 var weather = require('./public/lib/weatherData.js');
 var cresentials = require('./public/lib/credentials.js');
 var nodemailer = require('nodemailer');
+var router = require('./router');
 
 //nodemailer to set mail server
 var mailTransport = nodemailer.createTransport('SMTP', {
@@ -64,91 +65,91 @@ app.use(function (req, res, next) {
 	next();
 });
 
-
-app.post('/', function(req, res, err){
-	var name = req.body.name || '';
-	var email = req.body.email || '';
-	var reg = /\w+@\w+.com/ig;
-	if(!email.match(reg)){
-		console.log('---------1---------------');
-		if(req.xhr) return res.json({error : "Invalid name eamil address."});
-		req.session.flash = {
-			type: 'danger',
-			intro: req.body.email,
-			message: 'This eamil address you entered is not valid!'
-		};
-		return res.redirect(303, '/error/login_error');
-	}else{
-		return res.redirect(303, '/home');
-	}
-});
+app.use('/', router);
+// app.post('/', function(req, res, err){
+// 	var name = req.body.name || '';
+// 	var email = req.body.email || '';
+// 	var reg = /\w+@\w+.com/ig;
+// 	if(!email.match(reg)){
+// 		console.log('---------1---------------');
+// 		if(req.xhr) return res.json({error : "Invalid name eamil address."});
+// 		req.session.flash = {
+// 			type: 'danger',
+// 			intro: req.body.email,
+// 			message: 'This eamil address you entered is not valid!'
+// 		};
+// 		return res.redirect(303, '/error/login_error');
+// 	}else{
+// 		return res.redirect(303, '/home');
+// 	}
+// });
  
-app.get('/', function(req, res){
-	res.render('index');
-});
-app.get('/home', function (req, res) {
-	//console.log(res.locals.partials.weather.locations);
-	res.cookie('user', 'express cookie test');
-	res.render('home');
-	// res.render('partials/weather')
-});
-app.get('/about', function (req, res) {
-	res.render('about', {
-		fortune: fortune.getFortune(),
-		pageTestScript: '/qa/tests-about.js'
-	});
-});
-app.get('/error/login_error', function(req, res){
-	res.render('loginError');
-});
+// app.get('/', function(req, res){
+// 	res.render('index');
+// });
+// app.get('/home', function (req, res) {
+// 	//console.log(res.locals.partials.weather.locations);
+// 	res.cookie('user', 'express cookie test');
+// 	res.render('home');
+// 	// res.render('partials/weather')
+// });
+// app.get('/about', function (req, res) {
+// 	res.render('about', {
+// 		fortune: fortune.getFortune(),
+// 		pageTestScript: '/qa/tests-about.js'
+// 	});
+// });
+// app.get('/error/login_error', function(req, res){
+// 	res.render('loginError');
+// });
 
-app.get('/jquery', function (req, res) {
-	res.render('jqTest');
-});
-app.get('/tours/hood-river', function (req, res) {
-	res.render('tours/hood-river');
-});
-app.get('/tours/request-group-rate', function (req, res) {
-	res.render('tours/request-group-rate');
-});
-app.post('/cart/checkout', function(req, res){
-	console.log('get in cart confirm');
-	var cart = req.session.cart || {};
-	// if(!cart) next(new Error("cart dosn't exist"));
-	var name = req.body.name || "",
-		email = req.body.email || "";
-	var reg = /\w+@\w+.com/ig;
-	if(!email.match(reg)){
-		return res.next(new Error('Invalid email address.'));
-	}
-	cart.number = Math.random().toString().replace(/^0\.0*/, "");
-	cart.billing = {
-		name: name,
-		email: email
-	};
-	res.render('email/carThanks',{
-		layout: null,
-		cart: cart
-	}, function(err, html){
-		if(err){
-			console.log("error in email template");
-		}
-		mailTransport.sendMail({
-			from: "Meadowlark Travel : <leolewin@163.com>",
-			to: cart.billing.email,
-			subject: "test email for my learning on express",
-			html: html,
-			generateTextFromHtml: true
-		}, function(err){
-			if(err){
-				console.log("Unable to send confimation" + err.stack);
-			}
-		});
-	});
-	res.render('carThanks', {
-		cart: cart
-	});
-});
+// app.get('/jquery', function (req, res) {
+// 	res.render('jqTest');
+// });
+// app.get('/tours/hood-river', function (req, res) {
+// 	res.render('tours/hood-river');
+// });
+// app.get('/tours/request-group-rate', function (req, res) {
+// 	res.render('tours/request-group-rate');
+// });
+// app.post('/cart/checkout', function(req, res){
+// 	console.log('get in cart confirm');
+// 	var cart = req.session.cart || {};
+// 	// if(!cart) next(new Error("cart dosn't exist"));
+// 	var name = req.body.name || "",
+// 		email = req.body.email || "";
+// 	var reg = /\w+@\w+.com/ig;
+// 	if(!email.match(reg)){
+// 		return res.next(new Error('Invalid email address.'));
+// 	}
+// 	cart.number = Math.random().toString().replace(/^0\.0*/, "");
+// 	cart.billing = {
+// 		name: name,
+// 		email: email
+// 	};
+// 	res.render('email/carThanks',{
+// 		layout: null,
+// 		cart: cart
+// 	}, function(err, html){
+// 		if(err){
+// 			console.log("error in email template");
+// 		}
+// 		mailTransport.sendMail({
+// 			from: "Meadowlark Travel : <leolewin@163.com>",
+// 			to: cart.billing.email,
+// 			subject: "test email for my learning on express",
+// 			html: html,
+// 			generateTextFromHtml: true
+// 		}, function(err){
+// 			if(err){
+// 				console.log("Unable to send confimation" + err.stack);
+// 			}
+// 		});
+// 	});
+// 	res.render('carThanks', {
+// 		cart: cart
+// 	});
+// });
 
 //404
 app.use(function (req, res, next) {
